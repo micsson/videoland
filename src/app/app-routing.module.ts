a069c12a6099679d499d24f2250a5d6f8730e4b4
micsson <michaelperssonmail@gmail.com>
2019-05-29 15:24:44 +0200

    add default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MoviesComponent }      from './movies/movies.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
 
 
 const routes: Routes = [
-  { path: 'movies', component: MoviesComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
