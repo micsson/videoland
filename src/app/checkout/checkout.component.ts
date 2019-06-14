89312c80d5f67088adf70b7f15cc36fa2b98eadd
micsson <michaelperssonmail@gmail.com>
2019-06-14 11:13:47 +0200

    add cart item service and moment

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -3,6 +3,10 @@ import { Movie } from '../movie';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { MovieService }  from '../movie.service';
+import { CartItemService } from '../cart-item.service';
+import { IOrder } from '../Interfaces/IOrder';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-checkout',
@@ -17,6 +21,7 @@ export class CheckoutComponent implements OnInit {
     private route: ActivatedRoute,
     private movieService: MovieService,
     private location: Location,
+    public cartItemService: CartItemService
   ) { }
 
   ngOnInit() {
@@ -29,5 +34,32 @@ export class CheckoutComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  sendOrder(): void {
+    let orderRows = [];
+    for (let i = 0; i < this.cartItemService.cart.length; i++) {
+      orderRows.push({ productId: this.cartItemService.cart[i].movie.id, amount: this.cartItemService.cart[i].quantity});
+      
+    }
+
+    const companyId = 26;
+
+    let date = moment().format('YYYY-MM-DDTHH:mm:ss');
+
+    let order: IOrder = {
+      id: 0,
+      created: date,
+      createdBy: 'Micke',
+      totalPrice: 200,
+      status: 0,
+      paymentMethod: 'card',
+      orderRows: orderRows,
+      companyId: companyId,
+    }
+
+    this.movieService.addOrder(order).subscribe(result => {
+      console.log(result);
+    });
   }
 }
