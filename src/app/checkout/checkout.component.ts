4bd9eb45caba76f5ca4982c61cffdf47fdff1729
micsson <michaelperssonmail@gmail.com>
2019-06-17 19:00:14 +0200

    add personal information and payment method to checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -5,6 +5,7 @@ import { Location } from '@angular/common';
 import { MovieService }  from '../movie.service';
 import { CartItemService } from '../cart-item.service';
 import { IOrder } from '../Interfaces/IOrder';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
 
 import * as moment from 'moment';
 
@@ -17,11 +18,24 @@ export class CheckoutComponent implements OnInit {
 
   @Input() movie: Movie;
 
+  email = new FormControl('');
+
+  userInfo = new FormGroup({
+    userEmail: new FormControl('', Validators.required),
+  });
+
+  payment = new FormControl ('');
+
+  userPayment = new FormGroup({
+    paymentOption: new FormControl('', Validators.required),
+  })
+
   constructor(
     private route: ActivatedRoute,
     private movieService: MovieService,
     private location: Location,
-    public cartItemService: CartItemService
+    public cartItemService: CartItemService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
@@ -38,22 +52,25 @@ export class CheckoutComponent implements OnInit {
 
   sendOrder(): void {
     let orderRows = [];
+    let totalPrice = 0;
+
     for (let i = 0; i < this.cartItemService.cart.length; i++) {
       orderRows.push({ productId: this.cartItemService.cart[i].movie.id, amount: this.cartItemService.cart[i].quantity});
-      
+      totalPrice += this.cartItemService.cart[i].movie.price * this.cartItemService.cart[i].quantity;
     }
 
     const companyId = 26;
 
     let date = moment().format('YYYY-MM-DDTHH:mm:ss');
+    
 
     let order: IOrder = {
       id: 0,
       created: date,
-      createdBy: 'Micke',
-      totalPrice: 200,
+      createdBy: this.userInfo.value.userEmail,
+      totalPrice: totalPrice,
       status: 0,
-      paymentMethod: 'card',
+      paymentMethod: this.userPayment.value.paymentOption,
       orderRows: orderRows,
       companyId: companyId,
     }
